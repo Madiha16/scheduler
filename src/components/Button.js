8e5258a6a54fcce0f111e3b77bcdc4ae5e8f0357
Your Name <your@email.com>
2022-03-24 16:23:59 -0400

    add variable button text, conditional css, onClick and disabled props

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,32 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
+  let buttonClass = "button";
 
-   if (props.danger) {
-      buttonClass += " button--danger";
-    }
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  console.log("props:", props)
+  // {children: 'Clickable', onClick: ƒ}
+  //   children: "Clickable"
+  //   onClick: ƒ action()
  
-   return <button className={buttonClass}>{props.children}</button>;
+  return (
+    <button
+      onClick={props.onClick}
+      className={buttonClass}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
