5e41960373e297b4512981176d1c11a1832a40ef
Your Name <your@email.com>
2022-03-25 22:58:26 -0400

    conditionally add class if interviewer selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "components/InterviewerListItem.scss"
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
 
   console.log("InterviewerListItem >props::", props)
     // {id: 1, name: 'Sylvia Palmer', avatar: 'https://i.imgur.com/LpaY82x.png', selected: true}
     // {id: 1, name: 'Sylvia Palmer', avatar: 'https://i.imgur.com/LpaY82x.png', setInterviewer: ƒ}
   
   return (
-    <li onClick={() => props.setInterviwer(props.id)} className="interviewers__item">
+    <li onClick={() => props.setInterviwer(props.id)} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
