c49cd20ba233f6800edd7f59b9aae27e4358e668
Your Name <your@email.com>
2022-03-25 22:38:27 -0400

    replace hard-coded values with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "components/InterviewerListItem.scss"
 
-export default function InterviewerListItem() {
+export default function InterviewerListItem(props) {
+
+  // console.log("InterviewerListItem >props::", props)
+  //   {id: 1, name: 'Sylvia Palmer', avatar: 'https://i.imgur.com/LpaY82x.png', selected: true}
   
   return (
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
-
