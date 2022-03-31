3ff352d4819994b5b3908cc610efb75d05566d55
Your Name <your@email.com>
2022-03-31 07:18:57 -0400

    test request for interviewer data successful by logging value in Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,45 +5,6 @@ import Appointment from "./Appointment";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
 export default function Application(props) {
 
   const [state, setState] = useState({
@@ -54,57 +15,23 @@ export default function Application(props) {
   })
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  console.log("state::", state, "state.day::", state.day, "dailyAppointments::", dailyAppointments)
-    // appointments: {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}, 9: {…}, 10: {…}, 11: {…}, 12: {…}, 13: {…}, 14: {…}, 15: {…}, 16: {…}, 17: {…}, 18: {…}, 19: {…}, 20: {…}, 21: {…}, 22: {…}, 23: {…}, 24: {…}, 25: {…}}
-      //     1:
-      // id: 1
-      // interview: null
-      // time: "12pm"
-      // [[Prototype]]: Object
-      // 2: {id: 2, time: '1pm', interview: {…}}
-    // day: "Monday"
-    // days: (5) [{…}, {…}, {…}, {…}, {…}]
-        //     0: {id: 1, name: 'Monday', appointments: Array(5), interviewers: Array(5), spots: 2}
-        // 1:
-        // appointments: (5) [6, 7, 8, 9, 10]
-        // id: 2
-        // interviewers: (5) [3, 5, 7, 8, 10]
-        // name: "Tuesday"
-        // spots: 4
-        // [[Prototype]]: Object
-        // 2: {id: 3, name: 'Wednesday', appointments: Array(5), interviewers: Array(5), spots: 2}
-    // interviewers: {data: {…}, status: 200, statusText: 'OK', headers: {…}, config: {…}, …}
-        //     interviewers:
-        // config: {transitional: {…}, transformRequest: Array(1), transformResponse: Array(1), timeout: 0, adapter: ƒ, …}
-        // data: {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}, 9: {…}, 10: {…}}
-        // headers: {access-control-allow-origin: '*', content-length: '781', content-type: 'application/json; charset=utf-8', date: 'Thu, 31 Mar 2022 10:56:29 GMT', etag: 'W/"30d-3H0+hNLeoDR8/s+XemQgOCUbXbg"', …}
-        // request: XMLHttpRequest {onreadystatechange: null, readyState: 4, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}
-        // status: 200
-        // statusText: "OK"
-
-  // state.day
-    // Monday
-  // dailyAppointments
-    // 0: Array(5)
-      // 0: {id: 1, time: '12pm', interview: null}
-      // 1: {id: 2, time: '1pm', interview: {…}}
-      // 2: {id: 3, time: '2pm', interview: {…}}
-      // 3: {id: 4, time: '3pm', interview: null}
-      // 4:
-      // id: 5
-      // interview: {student: 'Jamal Jordan', interviewer: 8}
-      // time: "4pm"
+  // console.log("state::", state, "state.day::", state.day, "dailyAppointments::", dailyAppointments);
+  console.log("state.interviewers::", state.interviewers.data);
+    // {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}, 9: {…}, 10: {…}}
+    // 1: {id: 1, name: 'Sylvia Palmer', avatar: 'https://i.imgur.com/LpaY82x.png'}
+    // 2: {id: 2, name: 'Tori Malcolm', avatar: 'https://i.imgur.com/Nmx0Qxo.png'}
+    // 3: {id: 3, name: 'Mildred Nazir', avatar: 'https://i.imgur.com/T2WwVfS.png'}
+    // 4: {id: 4, name: 'Cohana Roy', avatar: 'https://i.imgur.com/FK8V841.jpg'}
+    // 5: {id: 5, name: 'Sven Jones', avatar: 'https://i.imgur.com/twYrpay.jpg'}
+    // 6: {id: 6, name: 'Susan Reynolds', avatar: 'https://i.imgur.com/TdOAdde.jpg'}
+    // 7: {id: 7, name: 'Alec Quon', avatar: 'https://i.imgur.com/3tVgsra.jpg'}
+    // 8: {id: 8, name: 'Viktor Jain', avatar: 'https://i.imgur.com/iHq8K8Z.jpg'}
+    // 9: {id: 9, name: 'Lindsay Chu', avatar: 'https://i.imgur.com/nPywAp1.jpg'}
+    // 10: {id: 10, name: 'Samantha Stanic', avatar: 'https://i.imgur.com/okB9WKC.jpg'}
+    // [[Prototype]]: Object
 
   const setDay = day => setState({ ...state, day });
 
-
-  // console.log("Application.js >> day, props::", day, props, Object.values(appointments))
-
-  // We are going to make a request to both endpoints at the same time.
-  // When both have returned their data, we need to update both parts of the state at the same time.
-  // We will use Promise.all to make both requests before updating the state.
-  // We'll pass Promise.all an array of promises. When all promises have resolved, we will receive an array
-  // of resolved values matching the order of the array passed to Promise.all.
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
@@ -117,13 +44,8 @@ export default function Application(props) {
         days: all[0].data,
         appointments: all[1].data,
         interviewers: all[2] }));
-      // console.log("all0:", all[0]); // days data
       // console.log("all0.d:",all[0].data); // days ARRAY
-
-      // console.log("all1:", all[1]); // appointments
       // console.log("all1.d:", all[1].data); // appointments OBJECT
-
-      // console.log("all2:", all[2]); // interviewers data
       // console.log("all2.d:", all[2].data); // interviewers OBJECT
     })
     .catch((error) => {
@@ -132,20 +54,6 @@ export default function Application(props) {
       console.log(error.response.data);
     });
   }, []);
-  // Could have saved returned promises to an array like Andy did
-  // then save each response to a variable:
-
-  // const promises = [recipePromise, ingredientPromise];
-
-  // Promise.all(promises)
-  //   .then((responseArr) => {
-  //     // console.log(responseArr);
-  //     const recipeResponse = responseArr[0];
-  //     setRecipes(recipeResponse.data);
-
-  //     setIngredients(responseArr[1].data);
-  //   });
-
 
   return (
     <main className="layout">
