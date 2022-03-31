439c5d9d6f6cedd93f22a2a49bbf9db3c4835ede
Your Name <your@email.com>
2022-03-31 06:44:18 -0400

    update effect to set days and appointments state at  same time

--- src/components/Application.js
+++ src/components/Application.js
@@ -71,15 +71,18 @@ export default function Application(props) {
     ]).then((all) => {
       // Now that we can access the data for days and appointments we need to update both in our state at the same time.
       // Update the effect to set the days and appointments state at the same time.
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2] }));
-      console.log("all0:", all[0]); // days data
-      console.log("all0.d:",all[0].data); // days ARRAY
+      setState(prev => ({...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2] }));
+      // console.log("all0:", all[0]); // days data
+      // console.log("all0.d:",all[0].data); // days ARRAY
 
-      console.log("all1:", all[1]); // appointments
-      console.log("all1.d:", all[1].data); // appointments OBJECT
+      // console.log("all1:", all[1]); // appointments
+      // console.log("all1.d:", all[1].data); // appointments OBJECT
 
-      console.log("all2:", all[2]); // interviewers data
-      console.log("all2.d:", all[2].data); // interviewers OBJECT
+      // console.log("all2:", all[2]); // interviewers data
+      // console.log("all2.d:", all[2].data); // interviewers OBJECT
     })
     .catch((error) => {
       console.log(error.response.status);
