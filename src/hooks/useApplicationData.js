04cc47f24eae38537cbb01b3ea91af2b94090a8c
Your Name <your@email.com>
2022-04-04 16:03:48 -0400

    clean up console logs

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -25,7 +25,6 @@ const useApplicationData = function(initial) {
 
     // spots are equal to the length of the null appointment array's length
     const spots = nullAptArr.length;
-    // console.log("spots=", spots);
 
     // set day equal to spread of dayObj, update spots
     const day = {...dayObj, spots };
@@ -42,8 +41,6 @@ const useApplicationData = function(initial) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      // Now that we can access the data for days and appointments we need to update both in our state at the same time.
-      // Update the effect to set the days and appointments state at the same time.
       setState(prev => ({...prev,
         days: all[0].data,
         appointments: all[1].data,
@@ -53,50 +50,33 @@ const useApplicationData = function(initial) {
 
 
   function bookInterview(id, interview) {
-    // console.log(id, interview);
-    // console.log("book interview function called with id, interview values!")
 
     const appointment = {
-      // going through all aptmts, get the one at [id]
       ...state.appointments[id],
-      // override the interview
       interview: { ...interview }
     };
 
-    // create new apt obj
     const appointments = {
-      // get all the aptmts from state, spread them out
       ...state.appointments,
-      // override the appointment with [id]
       [id]: appointment
     };
 
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(() => {
-        console.log("axios put sent");
         const days = updateSpots(state, appointments, id);
-        // setState({...state, appointments})
-        // When the response comes back we update the state using the existing setState
-        // Transition to SHOW when the promise returned by props.bookInterview resolves. 
         setState(prev => ({...prev, appointments, days }));
       })
   }
 
   function cancelInterview(id) {
-    // console.log("we need to delete this interview::", id);
 
     const appointment = {
-      // going through all aptmts, get the one at [id]
       ...state.appointments[id],
-      // override the interview
       interview: null
     };
 
-    // create new apt obj
     const appointments = {
-      // get all the aptmts from state, spread them out
       ...state.appointments,
-      // override the appointment with [id]
       [id]: appointment
     };
 
@@ -104,12 +84,9 @@ const useApplicationData = function(initial) {
 
     return axios.delete(`/api/appointments/${id}`)
       .then(() => {
-        // console.log('axios.delete successful');
         setState(prev => ({...prev, appointments, days }));
       })
   }
-
-  console.log("state:", state, "setDay:", setDay, "bookInterview:", bookInterview, "cancelInterview:", cancelInterview)
 
   return { state, setDay, bookInterview, cancelInterview };
 }
